339f8b70e52f65f6074560d81b07ea5ea6d74dad
Hector Daza <hdaza@precia.co>
2022-07-01 18:19:33 -0500

    New proyects

---
+++ angular-whishlist/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-whishlist';
+
+  agregar(titulo: HTMLInputElement){
+    console.log(titulo.value);
+  }
+}
